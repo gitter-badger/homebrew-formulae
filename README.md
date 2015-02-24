52359b4eb703f3d92fbb05422c82e024de07df30
Scott Stevenson <scott@stevenson.io>
2015-02-24 11:38:49 +0000

    latexrun head

---
+++ README.md
@@ -0,0 +1,7 @@
+Homebrew formulae
+=================
+
+Additional formulae for [Homebrew](http://brew.sh/).  Usage:
+
+    brew tap srstevenson/formulae
+    brew install <formula>
---
+++ latexrun.rb
@@ -0,0 +1,14 @@
+class Latexrun < Formula
+  homepage "https://github.com/aclements/latexrun"
+  head "https://github.com/aclements/latexrun.git"
+
+  depends_on :python3
+
+  def install
+    bin.install "latexrun"
+  end
+
+  test do
+    system "#{bin}/latexrun", "--help"
+  end
+end
